ed3c88f76e24812ef0a25ac743e4a77909fd72f9
fran-cisc <130452833+fran-cisc@users.noreply.github.com>
2023-04-17 09:41:26 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,71 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # . . .
+            . # . . #
+            . # # # .
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # # . . .
+            # # # . .
+            . . # . .
+            . . # # #
+            . . . . #
+            `)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            # # . . .
+            # # # # .
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            # . # . #
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . . # . .
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . # # # #
+            # # # # #
+            . . # . #
+            `)
+        basic.pause(1000)
+    }
+})
+basic.forever(function () {
+	
+})
